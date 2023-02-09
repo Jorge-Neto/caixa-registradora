884e3d56a5897071c48e87ce6ae619baff751bf8
Jorge-Neto <neto_tunas@hotmail.com>
2023-02-09 20:36:00 -0300

    feature(vuetify) Added vuetify on packages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { createApp } from 'vue/dist/vue.esm-bundler';
 import '@/style.css';
 import App from '@/App.vue';
-import router from '@/router';
 import store from '@/store';
+import router from '@/router';
 
-createApp(App).use(store).use(router).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+createApp(App).use(store).use(router).use(vuetify).mount('#app')

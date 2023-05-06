d1c5894c38bd373a2268bd1e610b099d49361189
Jorge-Neto <neto_tunas@hotmail.com>
2023-05-06 19:49:30 -0300

    fix(HeaderComponent) Fixed mobile view

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,8 +3,8 @@
 <template>
     <v-layout v-if="$route.name != 'NotFound'" class="top-bar">
         <v-app-bar class="top-bar-list">
-            <router-link class="ml-auto" to="/"><v-btn>Resumo do dia</v-btn></router-link>
-            <router-link class="mr-auto" to="/management"><v-btn>Produtos cadastrados</v-btn></router-link>
+            <router-link class="left-link" to="/"><v-btn>Resumo do dia</v-btn></router-link>
+            <router-link class="right-link" to="/management"><v-btn>Produtos cadastrados</v-btn></router-link>
         </v-app-bar>
     </v-layout>
 </template>
@@ -15,5 +15,26 @@
 }
 .top-bar-list {
     background-color: transparent !important;
+}
+.top-bar-list div {
+    flex-direction: column;
+}
+
+@media (max-width: 400px) {
+    .top-bar-list div {
+        height: unset !important;
+        margin-top: 2px;
+    }
+}
+@media (min-width: 401px) {
+    .top-bar-list div {
+        flex-direction: row;
+    }
+    .left-link {
+        margin-left: auto !important;
+    }
+    .right-link {
+        margin-right: auto !important;
+    }
 }
 </style>

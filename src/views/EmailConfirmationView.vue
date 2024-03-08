17e3f5b5aa6cac30056fc81bd62f68d0c648d2f2
Jorge-Neto <neto_tunas@hotmail.com>
2024-03-07 22:25:05 -0300

    feat(EmailConfirmationView) Added a page into views

---
+++ src/views/EmailConfirmationView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="container">
+        <h1>Confirmar e-mail</h1>
+        <p>Confirme o e-mail {{ $route.query.email }}</p>
+    </div>
+</template>
+
+<style></style>
